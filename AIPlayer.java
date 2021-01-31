cd9684c6810da13917ce583aaef240d69c713f80
robbiebmorris <robbiebmorris@gmail.com>
2021-01-31 21:56:51 +0800

    init psuedocode

---
+++ AIPlayer.java
@@ -0,0 +1,24 @@
+public class AIPlayer {
+
+  public void PlayerAI() {
+    // decide which type of ai to use
+  }
+
+  public void normalAI() {
+    // 1. if a winning move is available, take it
+    // 2. if the opponent is threatening a winning play, block it.
+    // 3. if the centre square is available, take it
+    // 4. else choose randomly between any remaining square
+
+  }
+
+  // normal minimax
+  public void miniMax(int depth) {
+
+  }
+
+  // minimax with alpha beta pruning
+  public void miniMax(int depth, int alpha, int beta) {
+
+  }
+}
