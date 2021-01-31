221eb74a295af4fa9a5a79e74254a514626ecc96
robbiebmorris <robbiebmorris@gmail.com>
2021-01-31 22:00:44 +0800

    TicTacToe outline

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,4 +1,28 @@
 public class TicTacToe {
+
+  // instance variables
+  private int[][] board;
+
+  public void board() {
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        board[i][j] = 0;
+      }
+    }
+  }
+
+  public boolean isGameOver() {
+    return true;
+  }
+
+  public boolean checkWinner() {
+    return true;
+  }
+
+  public void computerDifficulty(String difficulty) {
+
+  }
+
   public static void main(String[] args) {
 
   }
