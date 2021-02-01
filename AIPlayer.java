9a0fc690bae56275a81c9d07ca70740af81a65ec
robbiebmorris <robbiebmorris@gmail.com>
2021-02-01 14:46:39 +0800

    planning

--- AIPlayer.java
+++ AIPlayer.java
@@ -1,7 +1,7 @@
 public class AIPlayer {
 
-  public void PlayerAI() {
-    // decide which type of ai to use
+  public AIPlayer() {
+
   }
 
   public void normalAI() {
@@ -9,16 +9,6 @@ public class AIPlayer {
     // 2. if the opponent is threatening a winning play, block it.
     // 3. if the centre square is available, take it
     // 4. else choose randomly between any remaining square
-
-  }
-
-  // normal minimax
-  public void miniMax(int depth) {
-
-  }
-
-  // minimax with alpha beta pruning
-  public void miniMax(int depth, int alpha, int beta) {
 
   }
 }
