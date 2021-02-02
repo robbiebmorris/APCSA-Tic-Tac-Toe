55f4efe9d58d09d0bf7da7d75d2c5d717b519b26
robbiebmorris <robbiebmorris@gmail.com>
2021-02-03 00:23:37 +0800

    bugfixes and small tweaks

--- Board.java
+++ Board.java
@@ -1,7 +1,7 @@
 public class Board {
 
-  private Node[][] board;
-  int boardSize = 3;
+  Node[][] board;
+  static int boardSize = 3;
 
   public Board() {
     board = new Node[boardSize][boardSize];
@@ -9,10 +9,19 @@ public class Board {
   }
 
   public void newBoard() {
-    for (int row = 0; row < 3; row++) {
-      for (int col = 0; col < 3; col++) {
+    for (int row = 0; row < boardSize; row++) {
+      for (int col = 0; col < boardSize; col++) {
         board[row][col] = Node.EMPTY;
       }
+    }
+  }
+
+  public void printBoard() {
+    for (int row = 0; row < boardSize; row++) {
+      for (int col = 0; col < boardSize; col++) {
+        System.out.print(board[row][col].getNode());
+      }
+      System.out.println();
     }
   }
 
@@ -32,6 +41,18 @@ public class Board {
       }
     }
     return true;
+  }
+
+  public Node getNode(int row, int col) {
+    return this.board[row][col];
+  }
+
+  public void placeNode(int row, int col, boolean isX) {
+    if (isX) {
+      board[row][col] = Node.X;
+    } else {
+      board[row][col] = Node.O;
+    }
   }
 
   // for niall to do
