50b55029a201ca66f4e3cd824b23cc2e3728ba6f
NiallCordes <niallcordes@gmail.com>
2021-02-03 13:24:51 +0800

    init

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,5 +1,9 @@
 public class TicTacToe {
-  // for niall to do
-
+  // for niall to do - print out the board, choose the move, th
+//Static class designated for main - "hello, what difficulty do you want to play (normal or hard)"
+//Scanner "if scan = hard, this" "if scan = normal"
+     
+     
+     
   // class to run all the functions and make the game look pretty / aesthetic
 }
