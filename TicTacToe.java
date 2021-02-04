8be010ef8ef8e8450832e26b495b125c72a27689
robbiebmorris <robbiebmorris@gmail.com>
2021-02-04 19:43:35 +0800

    testing

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,9 +1,34 @@
+import java.util.*;
+
 public class TicTacToe {
   // for niall to do - print out the board, choose the move, th
-//Static class designated for main - "hello, what difficulty do you want to play (normal or hard)"
-//Scanner "if scan = hard, this" "if scan = normal"
-     
-     
-     
-  // class to run all the functions and make the game look pretty / aesthetic
+  // Static class designated for main - "hello, what difficulty do you want to
+  // play (normal or hard)"
+  // Scanner "if scan = hard, this" "if scan = normal"
+
+  public static void main(String[] args) {
+    Board board = new Board();
+    Scanner scan = new Scanner(System.in);
+    while (true) {
+      board.printBoard();
+      System.out.println();
+      while (true) {
+        int inputx = Integer.parseInt(scan.nextLine()) - 1;
+        int inputy = Integer.parseInt(scan.nextLine()) - 1;
+        if (board.isCellFull(inputx, inputy)) {
+          continue;
+        }
+        board.placeNode(inputx, inputy, Node.O);
+        break;
+      }
+      int[] ai = Minimax.move(board, Node.X);
+      board.placeNode(ai[0], ai[1], Node.X);
+      board.printBoard();
+      System.out.println();
+      if (board.isGameOver() || board.checkBoard() == Node.X || board.checkBoard() == Node.O) {
+        break;
+      }
+    }
+    scan.close();
+  }
 }
