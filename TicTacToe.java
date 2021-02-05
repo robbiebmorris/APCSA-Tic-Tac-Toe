39b4997fcf2969db6c8412af5b33cf1925b753eb
NiallCordes <niallcordes@gmail.com>
2021-02-05 10:23:44 +0800

    TicTacToe adjustment

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,22 +1,58 @@
 import java.util.*;
 
 public class TicTacToe {
-  // for niall to do - print out the board, choose the move, th
-  // Static class designated for main - "hello, what difficulty do you want to
-  // play (normal or hard)"
-  // Scanner "if scan = hard, this" "if scan = normal"
-
   public static void main(String[] args) {
     Board board = new Board();
     Scanner scan = new Scanner(System.in);
+
+    System.out.println("Welcome to Tic Tac Toe!");
     while (true) {
+      System.out.print("Choose a difficulty: ");
+      String difficulty = scan.nextLine();
       board.printBoard();
-      System.out.println();
-      playerMove(board, scan);
-      computerMove(board, "asd");
-      board.printBoard();
-      System.out.println();
-      if (board.isGameOver() || board.checkBoard() == Node.X || board.checkBoard() == Node.O) {
+      int xOrOs = (int) (Math.random() * (2 - 1 + 1) + 1);
+      System.out.println(xOrOs);
+      if (xOrOs == 1) {
+        System.out.println("You go first.");
+        playerMove(board, scan);
+        board.printBoard();
+      } else {
+        System.out.println("The computer plays first.");
+      }
+
+      while (true) {
+        System.out.println("Computer: ");
+
+        computerMove(board, difficulty);
+        board.printBoard();
+        if (board.checkBoard() == Node.X || board.checkBoard() == Node.O) {
+          System.out.println("You lost!");
+          break;
+        } else if (board.isGameOver()) {
+          System.out.println("It's a tie!");
+          break;
+        }
+
+        playerMove(board, scan);
+        board.printBoard();
+        if (board.checkBoard() == Node.X || board.checkBoard() == Node.O) {
+          System.out.println("You win!");
+          break;
+        } else if (board.isGameOver()) {
+          System.out.println("It's a tie!");
+          break;
+        }
+      }
+      System.out.println("Play again? (y/n)");
+      String playAgain = scan.nextLine();
+      if (playAgain.equalsIgnoreCase("y")) {
+        for (int row = 0; row < Board.boardSize; row++) {
+          for (int col = 0; col < Board.boardSize; col++) {
+            board.placeNode(row, col, Node.EMPTY);
+          }
+        }
+        continue;
+      } else {
         break;
       }
     }
@@ -25,9 +61,9 @@ public class TicTacToe {
 
   public static void playerMove(Board board, Scanner scan) {
     while (true) {
-      System.out.println("Input the x coordinate of your move: ");
+      System.out.print("Input the y coordinate of your move: ");
       int inputx = Integer.parseInt(scan.nextLine()) - 1;
-      System.out.println("Input the y coordinate of your move: ");
+      System.out.print("Input the x coordinate of your move: ");
       int inputy = Integer.parseInt(scan.nextLine()) - 1;
       if (board.isCellFull(inputx, inputy)) {
         continue;
@@ -40,11 +76,10 @@ public class TicTacToe {
   public static void computerMove(Board board, String difficulty) {
     int[] ai = new int[2];
     if (difficulty.equalsIgnoreCase("-a")) {
-      ai = PlayerAI.move(board, Node.X);
+      ai = PlayerAI.advancedAI(board);
     } else {
-      ai = PlayerAI.normalAI(board, Node.X); // unfinished function
+      ai = PlayerAI.normalAI(board, Node.X);
     }
     board.placeNode(ai[0], ai[1], Node.X);
-
   }
 }
