660f91625230fc415b8234f69d941bdf2ae0ca49
robbiebmorris <77032126+robbiebmorris@users.noreply.github.com>
2021-02-05 19:37:52 +0800

    comments

--- Node.java
+++ Node.java
@@ -1,16 +1,21 @@
+//enumerate class
 public enum Node {
+  // declare enumerate values
   X('X'), O('O'), EMPTY('-');
 
   private final char node;
 
+  // constructor method
   Node(char newNode) {
     this.node = newNode;
   }
 
+  // simple getter method
   public char getNode() {
     return this.node;
   }
 
+  // check if it doesn't equal empty
   public boolean isCellFull() {
     return this != EMPTY;
   }
